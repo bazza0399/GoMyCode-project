82390b89b7da0d6c1798435ef0f2dcb7dd19eea7
bazza0399 <58653894+bazza0399@users.noreply.github.com>
2020-09-04 14:29:58 +0100

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Main from './Main';
+import ChatBot from './ChatBot';
+import Team from './Team';
+import Gallery from './Gallery';
+const Home = ()=>{
+    return(
+        <>
+        <Main/>
+        <ChatBot/>
+        <Gallery/>
+        <Team/>
+        
+        </>
+    )
+}
+export default Home;
